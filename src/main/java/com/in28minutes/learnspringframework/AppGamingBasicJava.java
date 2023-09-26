eba2f453290b3e58f1d8bf5b422e530154087742
thakkardhavalc <thakkardhavalc@gmail.com>
2023-09-26 11:07:28 -0600

    added file

---
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingBasicJava.java
@@ -0,0 +1,10 @@
+package com.in28minutes.learnspringframework;
+
+/**
+ * Created By dhaval on 2023-09-26
+ */
+public class AppGamingBasicJava {
+    public static void main(String[] args) {
+
+    }
+}
